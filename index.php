1bcb8fb094adc3768893e5a0b6601d4ad92528f6
justin cardoza <jayymunoz66@gmail.com>
2015-04-14 12:21:12 -0700

    milestone 2 added index

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-index.php
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Justin's To-do List</title>
+	<link rel="stylesheet" type="text/css" href="">
+</head>
+<body
+<div>
+
+</div class"wrap">
+</body>
+</html>
