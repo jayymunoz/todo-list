a1ed936b2589fae2a535600b798933b3af18ba80
justin cardoza <jayymunoz66@gmail.com>
2015-04-16 12:10:41 -0700

    milestone 11

---
+++ new-task.php
@@ -0,0 +1,14 @@
+<?php
+$task = scrpit_tags($_POST['task']);
+$name = date('Y-m-d');
+$time = date('H:i:s');
+
+include('connect.php');
+
+$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+$mysqli ->query("INSERT INTO task VALUES('', '$task', '$date', '$time')");
+
+
+$query = "SELECT = FROM tasks WHERE task='$task' and date='$date' and time='$time' ";
+
+ ?>
