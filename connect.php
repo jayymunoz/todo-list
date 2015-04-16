079f76b87e077a1d809bae61b0c6a82fc0ef54b5
justin cardoza <jayymunoz66@gmail.com>
2015-04-15 19:39:56 -0700

    videos 2 3 4

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+//why is this important? why does localhost have to first?
+$mysqli = new mysqli('localhost', 'root', 'root', 'tasks'); 
+//$mysqli->connect_error then we want it to die on this message
+if ($mysqli->connect_error) {
+	die('Connection Error(' . $mysqli->connect_errno')'
+  . $mysqli->connect_error);
+}	
+else {
+	echo "Connection made";
+	#code...
+}
+ $mysqli->close();
+
+?> 
