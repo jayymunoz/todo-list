21774f16c3557e0a29c0f606cd901535b0af3f64
justin cardoza <jayymunoz66@gmail.com>
2015-04-15 20:21:15 -0700

    fixed error for 5.1

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 //why is this important? why does localhost have to first?
-$mysqli = new mysqli('localhost', 'root', 'root', 'tasks'); 
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo'); 
 //$mysqli->connect_error then we want it to die on this message
 if ($mysqli->connect_error) {
 	die('Connection Error(' . $mysqli->connect_errno')'
