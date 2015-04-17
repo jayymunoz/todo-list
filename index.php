403ffb6d1546a906783b173ee95f317ef2275bb5
justin cardoza <jayymunoz66@gmail.com>
2015-04-17 08:18:26 -0700

    echoed out task milestone 15

--- index.php
+++ index.php
@@ -8,7 +8,25 @@
 <div class"wrap">
   <div class="task-list">
   <ul>
-    <?php require("includes/connection.php"); ?>
+    <?php require("includes/connection.php");
+    $mysqli = new mysqli('loclahost', 'root', 'root', 'tasks');
+    $query = "SELECT * FROM tasks ORDER BY date ASC, time ASC";
+    if ($result = $mysqli->query($query)) {
+    	$numrows $result->rows;
+    	if ($numrows>8){
+    		while($row = $result->$fetch_assoc()){
+    			$task_id = $row['id'];
+    			$task_name = $row["task"];
+
+    			echo "<li>
+    			<span>'.$task_name'
+    			";
+    		}
+
+    	}
+
+    }
+    ?>
     </ul>
 </div>
 <form class="add-new-task" autocomplete="off">
